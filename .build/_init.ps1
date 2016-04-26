546dd7f91e94bb44f01db15bd34b1a4061e4865d
Chris Lambrou <github@chrislambrou.com>
2016-04-26 13:06:59 +0100

    Initial import.

---
+++ .build/_init.ps1
@@ -0,0 +1,66 @@
+# This file is a bootstrapper for the real build file. It's purpose is as follows:
+#
+# 1. Define some top-level functions (build, clean, rebuild) that can be used to kick off the build from the command-line.
+# 2. Download nuget.exe.
+# 3. Import the RedGate.Build module to make available some convenient build cmdlets.
+
+$VerbosePreference = 'Continue'          # Want useful output in our build log files.
+$ProgressPreference = 'SilentlyContinue' # Progress logging slows down TeamCity when downloading files with Invoke-WebRequest.
+$ErrorActionPreference = 'Stop'          # Abort quickly on error.
+
+function global:Build
+{
+    [CmdletBinding()]
+    param(
+        [string[]] $Task = @('Default')
+    )
+
+    Push-Location $PsScriptRoot -Verbose
+    try
+    {
+        # Obtain nuget.exe
+        $NuGetVersion = [version] '3.3.0'
+        $NuGetPath = '.\nuget.exe'
+        if (-not (Test-Path $NuGetPath) -or (Get-Item $NuGetPath).VersionInfo.ProductVersion -ne $NuGetVersion)
+        {
+            $NuGetUrl = "https://dist.nuget.org/win-x86-commandline/v$NuGetVersion/nuget.exe"
+            Write-Host "Downloading $NuGetUrl"
+            Invoke-WebRequest $NuGetUrl -OutFile $NuGetPath
+        }
+        
+        # Install the RedGate.Build module.
+        $RedGateBuildVersion = '0.1.162'
+        & $NuGetPath install 'RedGate.Build' `
+            -Version $RedGateBuildVersion `
+            -OutputDirectory 'packages' `
+            -PackageSaveMode nuspec
+
+        # Import the RedGate.Build module.
+        Import-Module ".\packages\RedGate.Build.$RedGateBuildVersion\tools\RedGate.Build.psm1" -Force
+
+        # Install Invoke-Build
+        $InvokeBuildDir = Install-Package -Name 'Invoke-Build' -Version '2.12.2'
+
+        # Call the actual build script.
+        & "$InvokeBuildDir\tools\Invoke-Build.ps1" -File '.\build.ps1' -Task $Task
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+function global:Clean 
+{
+    Build -Task Clean
+}
+
+function global:Rebuild
+{
+    Build -Task Rebuild
+}
+
+Write-Host 'This is the RedGate.ThirdParty.NodeJs repo. Here are the available commands:' -ForegroundColor Magenta
+Write-Host "    Build [-Task <task-list>]" -ForegroundColor Green
+Write-Host "    Clean" -ForegroundColor Green
+Write-Host "    Rebuild" -ForegroundColor Green
---
+++ .build/build.ps1
@@ -0,0 +1,94 @@
+# This file cannot be invoked directly; it simply contains a bunch of Invoke-Build tasks. To use it, invoke
+# _init.ps1 which declares three global functions (build, clean, rebuild), then invoke one of those functions.
+
+[CmdletBinding()]
+param([string]$Configuration = 'Release')
+
+use 14.0 MSBuild
+
+# Useful paths used by  tasks.
+$script:NodeJsDir = 'C:\Program Files\nodejs\'
+$script:RepositoryRoot = "$PsScriptRoot\.." | Resolve-Path
+$script:NuGetPath = "$PsScriptRoot\nuget.exe" | Resolve-Path
+$script:DistDir = "$RepositoryRoot\dist"
+
+# Helper function for clearer logging of each task.
+function Write-Info {
+    param ([string] $Message)
+    
+    Write-Host "## $Message ##" -ForegroundColor Magenta
+}
+
+task Init  {
+    # Check that the nodejs folder exists and contains node.exe.
+    if (-not (Test-Path $NodeJsDir)) {
+        throw "Cannot locate nodejs folder at $NodeJsDir"
+    }
+    $NodeJsPath = "$NodeJsDir\node.exe"
+    if (-not (Test-Path $NodeJsPath)) {
+        throw "node.exe file not found at $NodeJsPath"
+    }
+
+    # Extract the package version number from node.exe
+    $script:Version = (Get-Item $NodeJsPath).VersionInfo.ProductVersion
+    if (-not $Version) {
+        throw "Unable to retrieve the version number from $NodeJsPath"
+    }
+    Write-Info "Version = $Version"
+}
+
+
+# Clean task, deletes all build output folders.
+task Clean  {
+    Write-Info 'Cleaning build output'
+    
+    if (Test-Path $DistDir) {
+        Remove-Item $DistDir -Force -Recurse
+    }
+}
+
+
+# Bundles the nodejs files into a self-extracting archive.
+task Compress  Init, {
+    Write-Info 'Compressing nodejs files'
+
+    Start-Process -FilePath 'C:\Program Files\7-Zip\7z.exe' `
+                  -ArgumentList @('a', '-bb3', '-r', '-sfx', "$DistDir\nodejs-sfx.exe", "*") `
+                  -WorkingDirectory $NodeJsDir `
+                  -NoNewWindow `
+                  -Wait
+}
+
+
+task Package  Compress,  {
+    Write-Info 'Generating NuGet package'
+
+    # Temporarily insert the node version number in to the props file.
+    $PropsFilePath = "$RepositoryRoot\RedGate.ThirdParty.NodeJs.props"
+    $Encoding = New-Object 'System.Text.UTF8Encoding' $False
+    $OriginalContents = [System.IO.File]::ReadAllText($PropsFilePath, $Encoding)
+    $NewContents = $OriginalContents -replace '(?<=\<NodeJsVersion\>).*?(?=\</NodeJsVersion\>)', $Version.ToString()
+    [System.IO.File]::WriteAllText($PropsFilePath, $NewContents, $Encoding)
+
+    # Run NuGet pack.
+    try {
+        $NuSpecPath = "$RepositoryRoot\RedGate.ThirdParty.NodeJs.nuspec" | Resolve-Path
+        $Parameters = @(
+            'pack',
+            "$NuSpecPath",
+            '-Version', $Version,
+            '-OutputDirectory', $DistDir
+        )
+        & $NuGetPath $Parameters
+    }
+    
+    # Reinstate the original props file.
+    finally {
+        [System.IO.File]::WriteAllText($PropsFilePath, $OriginalContents, $Encoding)
+    }
+}
+
+
+task Build  Package
+task Rebuild  Clean, Build
+task Default  Build
